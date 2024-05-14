caf409b5d71680dc564c02c27eb660e5e26532d4
Your <rlawlsdn1020@gmail.com>
2024-05-14 17:47:41 +0900

    0514포트폴리오

--- js/index.js
+++ js/index.js
@@ -8,26 +8,26 @@ let hd = () => {
 $(" ul > li").mouseenter(function () {
   $(this).children(".menu-sub-container").stop().slideDown(400);
   $(this).children(".menu-sub-container").show();
-  $(".menu-container").css({
-    "background-color": "white",
-  });
-  $(".menu").css({
-    color: "rgb(184, 3, 123)",
-  });
-  $(this).children(".menu-sub-container").css({
-    "background-color": "white",
-  });
+  // $(".menu-container").css({
+  //   "background-color": "white",
+  // });
+  // $(".menu").css({
+  //   color: "rgb(184, 3, 123)",
+  // });
+  // $(this).children(".menu-sub-container").css({
+  //   "background-color": "white",
+  // });
 });
 $(" ul > li").mouseleave(function () {
   $(this).children(".menu-sub-container").stop().slideUp(100);
-  $(".menu-container").css({
-    "background-color": "rgb(184, 3, 123)",
-  });
-  $(".menu").css({
-    color: "white",
-  });
-  $(this).children(".menu-sub-container").css({
-    "background-color": "rgb(184, 3, 123)",
-  });
+  // $(".menu-container").css({
+  //   "background-color": "rgb(184, 3, 123)",
+  // });
+  // $(".menu").css({
+  //   color: "white",
+  // });
+  // $(this).children(".menu-sub-container").css({
+  //   "background-color": "rgb(184, 3, 123)",
+  // });
 });
 hd();
